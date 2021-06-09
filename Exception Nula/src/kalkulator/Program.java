35627a766d8cb101a5cd73c0678518d7a1971086
Josip-github <josipcota1@gmail.com>
2021-06-09 11:42:16 +0200

    Integer.parseInt i JOptionPane radi uključivanja korisnika u program

--- Exception Nula/src/kalkulator/Program.java
+++ Exception Nula/src/kalkulator/Program.java
@@ -1,8 +1,28 @@
 package kalkulator;
 
+import javax.swing.JOptionPane;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
+		int operacija = Integer.parseInt(
+				JOptionPane.showInputDialog("Dobrodošli u kalkulator!\n"
+						+ "Upišite 1 za zbrajanje, 2 za odiuzimanje\n"
+						+ "3 za množenje ili 4 za dijeljenje!\n"
+						+ "Ako upišete neki drugi broj, program se neæe nastaviti.")
+				);
+		
+		int unosKorisnika1 = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite jedan cijeli broj!")
+				);
+		
+		int unosKorisnika2 = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesite jedan cijeli broj!")
+				);
+		
+		
+		
 		
 		Kalkulator.zbrajanje(20, 6758);
 		
