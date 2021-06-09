21cdb9194ea3a235dab0285d527ad6845b813bf9
Josip-github <josipcota1@gmail.com>
2021-06-09 11:14:07 +0200

    definiranje metoda oduzimanje, množenje i dijeljenje

--- Exception Nula/src/kalkulator/Kalkulator.java
+++ Exception Nula/src/kalkulator/Kalkulator.java
@@ -7,4 +7,16 @@ public class Kalkulator {
 	public static void zbrajanje(int broj1, int broj2) {
 		System.out.println(broj1 + broj2);
 	}
+	
+	public static void oduzimanje(int broj1, int broj2) {
+		System.out.println(broj1 - broj2);
+	}
+	
+	public static void množenje(int broj1, int broj2) {
+		System.out.println(broj1 * broj2);
+	}
+	
+	public static void dijeljenje(int broj1, int broj2) {
+		System.out.println(broj1 / broj2);
+	}
 }
