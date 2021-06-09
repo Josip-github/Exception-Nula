9b3df84e6ed79991d6fe75ebe7857e75bf54dce9
Josip-github <josipcota1@gmail.com>
2021-06-09 11:25:52 +0200

    Stvaranje main metode u novoj klasi, pozivanje svih metoda osim dijeljenja

---
+++ Exception Nula/src/kalkulator/Program.java
@@ -0,0 +1,15 @@
+package kalkulator;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Kalkulator.zbrajanje(20, 6758);
+		
+		Kalkulator.oduzimanje(4769, 4632);
+		
+		Kalkulator.množenje(34, 12);
+
+	}
+
+}
