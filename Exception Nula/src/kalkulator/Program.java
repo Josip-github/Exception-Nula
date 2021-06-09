59113f481af63c786a8dd422b6351056cd379961
Josip-github <josipcota1@gmail.com>
2021-06-09 11:52:18 +0200

    If statements. Korisnik unosom sam bira koju operaciju želi i koje brojeve

--- Exception Nula/src/kalkulator/Program.java
+++ Exception Nula/src/kalkulator/Program.java
@@ -5,36 +5,37 @@ import javax.swing.JOptionPane;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		int operacija = Integer.parseInt(
 				JOptionPane.showInputDialog("Dobrodošli u kalkulator!\n"
-						+ "Upišite 1 za zbrajanje, 2 za odiuzimanje\n"
-						+ "3 za množenje ili 4 za dijeljenje!\n"
-						+ "Ako upišete neki drugi broj, program se neæe nastaviti.")
+				+ "Upišite 1 za zbrajanje, 2 za oduzimanje\n" 
+				+ "3 za množenje ili 4 za dijeljenje!\n"
+				+ "Ako upišete neki drugi broj, program se neæe nastaviti.")
 				);
-		
+
 		int unosKorisnika1 = Integer.parseInt(
 				JOptionPane.showInputDialog("Unesite jedan cijeli broj!")
 				);
-		
+
 		int unosKorisnika2 = Integer.parseInt(
 				JOptionPane.showInputDialog("Unesite jedan cijeli broj!")
 				);
-		
-		
-		
-		
-		Kalkulator.zbrajanje(20, 6758);
-		
-		Kalkulator.oduzimanje(4769, 4632);
-		
-		Kalkulator.množenje(34, 12);
-		
-		try {
-			Kalkulator.dijeljenje(20, 0);
-		} catch (ExceptionNeDijelitiSNulom e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		if (operacija < 1 | operacija > 4) {
+			System.out.println("Program se neæe nastaviti, krivi unos.");
+		} else if (operacija == 1) {
+			Kalkulator.zbrajanje(unosKorisnika1, unosKorisnika2);
+		} else if (operacija == 2) {
+			Kalkulator.oduzimanje(unosKorisnika1, unosKorisnika2);
+		} else if (operacija == 3) {
+			Kalkulator.množenje(unosKorisnika1, unosKorisnika2);
+		} else if (operacija == 4) {
+			try {
+				Kalkulator.dijeljenje(unosKorisnika1, unosKorisnika2);
+			} catch (ExceptionNeDijelitiSNulom e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 	}
