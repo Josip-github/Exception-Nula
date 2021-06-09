1b9ef1dd9c9b06801e4c68270e6a3c44a12f34f4
Josip-github <josipcota1@gmail.com>
2021-06-09 11:28:06 +0200

    Pozivanje metode dijeljenje i zatim zbog Exception jako je važno i nužno izraditi try/catch block, najjednostavnije i najbrže je doći klikom miša na "dijeljenje" i zatim ga Eclipse sam ponudi

--- Exception Nula/src/kalkulator/Program.java
+++ Exception Nula/src/kalkulator/Program.java
@@ -9,6 +9,13 @@ public class Program {
 		Kalkulator.oduzimanje(4769, 4632);
 		
 		Kalkulator.množenje(34, 12);
+		
+		try {
+			Kalkulator.dijeljenje(20, 0);
+		} catch (ExceptionNeDijelitiSNulom e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
